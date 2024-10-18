5524cdc506a9ebd459e2f0b5bac8531646dad19e
kaiden-ong <kaiden.ong000@gmail.com>
2024-10-18 01:00:00 -0700

    adding audio

--- STT.py
+++ STT.py
@@ -1,5 +1,6 @@
 import speech_recognition as sr
 import json
+from playsound import playsound
 
 def record_text(r):
     while(1):
@@ -27,17 +28,22 @@ def main(door_status, pw):
     r = sr.Recognizer()
     while(1):
         text = record_text(r)
+        if text == "stop":
+            return None
         if door_status["status"] == "unlocked":
             if text == "lock door":
                 door_status["status"] = "locked"
                 return False
             else:
+                playsound("./audio/alr_unlocked.mp3")
                 print("door is already unlocked")
-                return True;
+                return None
         else:
+            if text == "lock door":
+                playsound("./audio/alr_locked.mp3")
+                print("the door is already locked")
+                return None
             output_text(text)
-            if text == "stop":
-                break
             if text == "hey sound pass":
                 password = record_text(r)
                 output_text("password attempt: " + password)
@@ -50,11 +56,15 @@ if __name__=="__main__":
         password = data["password"]
     with open("output.txt", "w") as f:
         pass
-    if main(door_status, password):
-        door_status["status"] = "unlocked"
-        print("door unlocks")
-    else:
-        print("door locks")
+    mainReturn = main(door_status, password)
+    if mainReturn != None:
+        if mainReturn:
+            door_status["status"] = "unlocked"
+            playsound("./audio/unlocking.mp3")
+            print("door unlocks")
+        else:
+            playsound("./audio/locking.mp3")
+            print("door locks")
     data["door_status"] = door_status["status"]
     with open('door.json', 'w') as file:
         json.dump(data, file)
